69ebae914734aa8d9d257d499cc2c199858e26b1
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-09-18 23:32:08 -0500

    se comienza con slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { Banner } from './componentes/Banner'
+import { SliderCarousel } from './componentes/Carousel/Slider'
 
 
 function App() {
 
   return (
     <>
-     
       <Banner></Banner>
+      <SliderCarousel></SliderCarousel>
     </>
   )
 }
---
+++ src/componentes/Carousel/Slider.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Box from '@mui/material/Box';
+import Slider from 'react-slick';
+
+export const SliderCarousel = () => {
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    return (
+        <Box
+            sx={{
+                padding: '1rem',
+                color: 'white',
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            }}>
+            <h2>Slider</h2>
+            <Slider  className="slider-box" {...settings}>
+                <div className="box">
+                    <div className="box-titulo">1</div>
+                </div>
+                <div className="box">
+                    <div className="box-titulo">2</div>
+                </div>
+                <div className="box">
+                    <div className="box-titulo">3</div>
+                </div>
+                <div className="box">
+                    <div className="box-titulo">4</div>
+                </div>
+                <div className="box">
+                    <div className="box-titulo">5</div>
+                </div>
+            </Slider>
+        </Box>
+    )
+}
